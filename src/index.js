c1430e729e39abbf31d0bb2f17f79dfa2a8e2eda
Bumhan Yu <bumhan.yu@gmail.com>
2019-07-12 23:24:44 -0400

    WIP: tutorial part 6

--- src/index.js
+++ src/index.js
@@ -11,11 +11,21 @@ const config = {
   parent: 'phaser-example',
   width: 800,
   height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
   scene: {
     preload,
     create,
   },
 };
+
+let platforms;
+let player;
 
 const game = new Phaser.Game(config);
 
@@ -29,8 +39,36 @@ function preload() {
 
 function create() {
   this.add.image(400, 300, 'sky');
-  this.add.image(400, 300, 'star');
-  // const ground = this.add.image(400, 150, 'ground');
-  // const star = this.add.image(400, 150, 'star');
-  // const bomb = this.add.image(400, 150, 'bomb');
+  platforms = this.physics.add.staticGroup();
+  platforms
+    .create(400, 568, 'ground')
+    .setScale(2)
+    .refreshBody();
+  platforms.create(600, 400, 'ground');
+  platforms.create(50, 250, 'ground');
+  platforms.create(750, 220, 'ground');
+
+  player = this.physics.add.sprite(100, 450, 'dude');
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+  player.body.setGravityY(300);
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: 'turn',
+    frames: [{ key: 'dude', frame: 4 }],
+    frameRate: 20,
+  });
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1,
+  });
 }
